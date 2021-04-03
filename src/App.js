c79cd538ff9fe6ad652da49af21dfd80fc688b89
CamilaDelagnese <camiladelagnese22@gmail.com>
2021-04-03 10:16:39 +0100

    add a button

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import "./App.css";
 export default function App() {
   return (
   <div className="App">
-   <h1> Weather App  </h1>  
+   <h1> Weather App  </h1> 
+   <button className="btn btn-primary" >Hello!</button>
    <footer>
      This project was coded by{" "}
       <a href="https://github.com/CamilaDelagnese/react-app"

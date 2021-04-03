9a5e70a972864de9d14274955e2bf6f56b1dc75f
CamilaDelagnese <camiladelagnese22@gmail.com>
2021-04-03 16:21:23 +0100

    add basic css

--- src/App.js
+++ src/App.js
@@ -6,16 +6,19 @@ export default function App() {
   return (
   <div className="App">
     <div className="container">
-   <h1>Weather App</h1>
+   
    <Weather/>
  
    <footer>
      This project was coded by{" "}
+     <a href=" " target="_blank">
+      Camila delagnese 
+      </a>{" "}
+      and is{" "}
       <a href="https://github.com/CamilaDelagnese/react-app"
-          target="_blank"
-          rel="noreferrer"> Camila delagnese</a>{" "}
-          open-sourced on GitHub
-          </footer>
+          target="_blank" rel="noreferrer"> 
+          open-sourced on GitHub </a>
+  </footer>
           </div>
           </div>);}
 

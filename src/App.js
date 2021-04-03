373e62a46c264aaba9b3b77d5a578528df6caae0
CamilaDelagnese <camiladelagnese22@gmail.com>
2021-04-03 10:07:47 +0100

    add some changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
-import './App.css';
+import "./App.css";
 
 export default function App() {
-  return <div className="App">
-    Weather App         <a
-          href="https://github.com/CamilaDelagnese/react-app"
+  return (
+  <div className="App">
+   <h1> Weather App  </h1>  
+   <footer>
+     This project was coded by{" "}
+      <a href="https://github.com/CamilaDelagnese/react-app"
           target="_blank"
-          rel="noreferrer"
-        >
-          Open-source code
-        </a>
-        by Camila Delagnese
-      </small> </div>;
-}
+          rel="noreferrer"> Camila delagnese</a>{" "}
+          open-sourced on GitHub
+          </footer>
+          </div>);}
+
+        
+
